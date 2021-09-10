2af93295b4cc667bb142c4d9dac8d52aa0daa408
Stephen <stevriss22@gmail.com>
2021-09-10 14:12:10 -0400

    added brief intro div and base for superbowl calculator

---
+++ jawaScript.js
@@ -0,0 +1,40 @@
+//joke teller
+function nflSuperBowls(team){
+    const teams = [
+        'Patriots',
+        'Steelers',
+        '49ers',
+        'Cowboys',
+        'Packers',
+        'Giants',
+        'Raiders',
+        'Broncos',
+        'Washington',
+        'Chiefs',
+        'Colts',
+        'Dolphins',
+        'Ravens',
+        'Buccaneers',
+        'Bears',
+        'Saints',
+        'Rams',
+        'Jets',
+        'Seahawks',
+        'Eagles',
+        'Bills',
+        'Browns',
+        'Vikings',
+        'Cardinals',
+        'Lions',
+        'Texans',
+        'Jaguars',
+        'Titans',
+        'Chargers',
+        'Bengals',
+        'Falcons',
+    ];
+
+    if (document.getElementById("team")=== 'Patriots'){
+        console.log('6');
+    }
+}
